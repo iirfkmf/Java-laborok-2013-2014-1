8c132851710c48989e03665de0163aea86aa34cc
beistvan <beistvan@gmail.com>
2013-10-03 09:31:17 +0200

    Create HelloVilagJava.java
    Helló világ, az első Java program

---
+++ HelloVilagJava.java
@@ -0,0 +1,18 @@
+/*
+ * Helló világ, az első Java program
+ */
+
+package hellovilagjava;
+
+/**
+ * @author Beregszászi IStván
+ * @since 2013.10.03.
+ */
+public class HelloVilagJava {
+
+    public static void main(String[] args) {
+        // A "Helló világ!" szöveg kiíratása
+        System.out.println("Helló világ!");
+    }
+
+}
